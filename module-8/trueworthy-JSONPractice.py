2c5c1425615613b1587a72139f3c721fb84ddf30
trueworthy <ltrueworthy@yahoo.com>
2024-12-07 09:01:25 -0700

    work in progess

---
+++ module-8/trueworthy-JSONPractice.py
@@ -0,0 +1,42 @@
+# trueworthy_JSONPractice
+
+import json
+
+with open("student.json") as f:
+    class_list = json.load(f)
+
+print(type(class_list))
+
+import json
+
+def list_of_students(class_list):
+    for student in class_list:
+        print(f"{student['F_Name']} , {student['L_Name']} : ID = {student['Student_ID']} , Email = {student['Email']}")
+
+    with open("student.json", "r") as read_file:
+        class_list = json.load(read_file)
+
+print("This is the original student list:")
+print(list_of_students(class_list))
+
+new_student = {"F_Name": "Lea",
+               "L_Name": "Trueworthy",
+               "Student_ID": "80228",
+               "Email": "ltrueworthy@my365.bellevue.edu"
+               }
+
+with open("student.json", "w") as f:
+    json.dump(new_student, f, indent=4)
+
+class_list.append(new_student)
+
+# Print updated student list
+print("\nThis is the updated student list:")
+list_of_students(class_list)
+
+# Update the JSON file with new data
+with open('student.json', 'w') as f:
+    json.dump(class_list, f, indent=4)
+
+# Notify user that the file has been updated
+print("\nThe student data in the JSON file has been updated.")
